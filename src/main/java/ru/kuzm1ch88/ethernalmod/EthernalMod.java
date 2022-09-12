cab25e3d4f8ab65853eb905c9527ad6e91659e7e
Kuzm1ch88 <steam.danya@yandex.ru>
2022-09-12 13:03:49 +0300

    First item

--- src/main/java/ru/kuzm1ch88/ethernalmod/EthernalMod.java
+++ src/main/java/ru/kuzm1ch88/ethernalmod/EthernalMod.java
@@ -3,19 +3,15 @@ package ru.kuzm1ch88.ethernalmod;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.kuzm1ch88.ethernalmod.item.EthernalItems;
 
 public class EthernalMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "ethernalmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
+		EthernalItems.registerEthernalItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/ru/kuzm1ch88/ethernalmod/item/EthernalItems.java
@@ -0,0 +1,26 @@
+package ru.kuzm1ch88.ethernalmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import ru.kuzm1ch88.ethernalmod.EthernalMod;
+
+public class EthernalItems {
+
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(EthernalMod.MOD_ID, name), item);
+    }
+
+    public static void registerEthernalItems(){
+        EthernalMod.LOGGER.info("Registering Mod Items for " + EthernalMod.MOD_ID);
+    }
+}
